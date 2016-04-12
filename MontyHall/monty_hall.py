837416d8537350beb7ca8e9f12e4608396a391c2
bijjr <bjffrys@gmail.com>
2016-04-12 08:49:27 -0400

    Refactoring

--- MontyHall/monty_hall.py
+++ MontyHall/monty_hall.py
@@ -1,16 +1,6 @@
-import os
 import random
-import statistics
 
-# def run_again():
-# 	run = int(input("Would you like to simulate again? "))
-# 	if run == 1:
-# 		main()
-# 	else:
-# 		exit()
-
-def print_stats(win,loss):
-	
+def print_stats(win,loss):	
 	win_perc = "%.2f" % (float(win / 1000))
 	loss_perc = "%.2f" % (float(loss / 1000))
 
@@ -30,7 +20,7 @@ def remove_door(doors, pd, sd):
 
 def game():	
 	win,loss = 0, 0
-	for i in range(1000):
+	for _ in range(1000):
 		doors = [1,2,3]
 		pd = random.choice(doors)
 		print("prized door {}".format(pd))
@@ -38,32 +28,17 @@ def game():
 		print("selected door {}".format(sd))
 		rd = remove_door(doors,pd,sd)
 		print("removeddoor {}".format(rd))
-		# sd = switch_door(rd, sd)
-		# print("switched to door {}".format(sd))
+		sd = switch_door(rd, sd)
+		print("switched to door {}".format(sd))
 		print("=========")
 		if sd == pd:
 			win += 1
 		else:
 			loss += 1
-	print_stats(win,loss)
-
-	# switch_door(remove_door, select_door)
-	
+	print_stats(win,loss)	
 
 def main():
-
 	game()
-
-	# run_again()
-
 
 if __name__ == '__main__':
 	main()
-
-#///////////////////////////////////////////////////////////////////////////
-
-
-# class Game():
-# 	def __init__():
-# 		self.
-# 		self.
